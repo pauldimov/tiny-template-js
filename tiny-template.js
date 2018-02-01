c27be7ee1893f3ad916ca1cdb62eb181ec9d0dda
Pavel Dimov <ad.opus@gmail.com>
2018-02-01 15:01:14 +0300

    Added basic tiny template library

---
+++ tiny-template.js
@@ -0,0 +1,54 @@
+//! tiny-template.js
+//! version : 1.0.0
+//! authors : Pavel Dimov
+//! license : MIT
+
+var tt = function () { };
+
+tt.Obj = function(templateID, obj) {
+	var html = null;
+	if(!templateID || templateID.length == '')
+		return '';
+	if(templateID.length < 100)
+		html = document.getElementById(templateID).innerHTML;
+
+	if(!html)
+		html = templateID;
+
+	with (obj) {
+		html = html.replace(/\{{(.*?)\}}/g, function (g0, g1) {
+			var value = '';
+			try {
+				value = eval(g1);
+			} catch (e) {
+				if (e instanceof SyntaxError) {
+					value = '<span style="color:red;">' + g1 + ' &gt;&gt; ' + '<b>' + e.message + '</b>' + '</span>';
+					if (console && console.error) {
+						console.error(e.stack);
+					}
+				} else {
+					value = '<span style="color:red;">' + g1 + ' &gt;&gt; ' + '<b>' + (e.message ? e.message : '') + '</b>' + '</span>';
+					if (console && console.error && e.stack) {
+						console.error(e.stack);
+					}
+				}
+			}
+			return value;
+		})
+	}
+	return html;
+};
+
+tt.Array = function (templateID, obj) {
+	var htmlTemplate = document.getElementById(templateID).innerHTML;
+	var result = '';
+	if (obj instanceof Array) {
+		for (var i = 0; i < obj.length; i++) {
+			result += this.Obj(htmlTemplate, obj[i]);
+		}
+	} else {
+		result += this.Obj(templateID, obj);
+	}
+
+	return result;
+};
